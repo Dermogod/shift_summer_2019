b5b7a19182ff70dc78cc7ae82def7a0677ab773a
ODB686 <griboed228@gmail.com>
2019-07-02 22:16:42 -0400

    try to fix ssti

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -14,12 +14,12 @@ def unsafe_ssti():
 
 	person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'}
 
-	if person['name'] is None:
-		person['name'] = 'world!'
-
 	body = '''<title>No Injection Allowed!</title>
                 <a href={{ url_for('hello_xss')}}?person={{person |e}}>
                 Click here for a welcome message</a>'''
+        
+        if person['name'] is None:
+            person['name']='world!'
 
 	return render_template_string(body, person=person)
 
