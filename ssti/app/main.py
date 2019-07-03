4c9692aad1c7c736327edc656de7bbf4b96f1d0a
ODB686 <griboed228@gmail.com>
2019-07-03 06:04:18 -0400

    dada

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -21,7 +21,17 @@ def unsafe_ssti():
 
 	return render_template_string(body, person=person)
 
+@app.route("/safe")
+def safe_ssti():
+	
+	person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'} 
 
+	if person['name'] is None:
+		person['name'] = 'world!'
+
+	body = "Name: %s" % person['name']
+
+	return render_template_string(body, person=person)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True, port=80)
