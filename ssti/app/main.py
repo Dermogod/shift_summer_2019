6fda518aae368080ed56fabd8dd24205e824850f
ODB686 <griboed228@gmail.com>
2019-07-04 02:28:22 -0400

    da

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -25,10 +25,9 @@ def unsafe_ssti():
 @app.route('/safe_ssti')
 def safe_ssti():
 	name = "world"
-	template = 'hello.unsafe' # 'unsafe' file extension... totally legit.
 	if request.args.get('name'):
 		name = request.args.get('name')
-	return render_template(template, name=name)
+	return render_template('thisisafe', name=name)
 
 
 if __name__ == '__main__':
