045077aaf5c7f6094dd6aca3a67816c59a29c1fb
ODB686 <griboed228@gmail.com>
2019-07-03 22:30:55 -0400

    da

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -12,7 +12,7 @@ def hello():
 @app.route("/unsafe")
 def unsafe_ssti():
 
-	person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'} 
+    person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'} 
 
 	if person['name'] is None:
 		person['name'] = 'world!'
